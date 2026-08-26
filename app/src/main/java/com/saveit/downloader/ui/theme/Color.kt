09ae3d129add5bdc122db66fafcba4dbd044f258
mq742036-creator <mq742036@gmail.com>
2026-08-26 22:31:49 +0500

    Add color definitions for SaveIt app theme
    Defines a set of color values for the SaveIt app theme, including primary, secondary, and gradient colors.

---
+++ app/src/main/java/com/saveit/downloader/ui/theme/Color.kt
@@ -0,0 +1,35 @@
+package com.saveit.downloader.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+// Custom SaveIt colors
+val SaveItPrimary = Color(0xFF6C5CE7)
+val SaveItPrimaryDark = Color(0xFF5A4BD1)
+val SaveItSecondary = Color(0xFF00CEC9)
+val SaveItAccent = Color(0xFFFD79A8)
+val SaveItBackground = Color(0xFF0A0A0F)
+val SaveItSurface = Color(0xFF1A1A2E)
+val SaveItSurfaceVariant = Color(0xFF2D2D44)
+val SaveItText = Color(0xFFF5F6FA)
+val SaveItTextSecondary = Color(0xFFA0A0B8)
+val SaveItSuccess = Color(0xFF00B894)
+val SaveItError = Color(0xFFE17055)
+val SaveItWarning = Color(0xFFFDCB6E)
+
+// Gradient colors
+val GradientStart = Color(0xFF6C5CE7)
+val GradientMiddle = Color(0xFFA29BFE)
+val GradientEnd = Color(0xFF00CEC9)
+
+// Quality colors
+val Quality480p = Color(0xFF0984E3)
+val Quality720p = Color(0xFF00B894)
+val Quality1080p = Color(0xFFE17055)
