24844a41b37f7b01595990f4144f6c25d5818c3a
mq742036-creator <mq742036@gmail.com>
2026-08-30 00:42:11 +0500

    Add ExperimentalMaterial3Api annotation to VideoPlayerScreen

--- app/src/main/java/com/saveit/downloader/ui/screens/VideoPlayerScreen.kt
+++ app/src/main/java/com/saveit/downloader/ui/screens/VideoPlayerScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
@@ -28,6 +29,7 @@ import com.saveit.downloader.ui.theme.SaveItPrimary
 import com.saveit.downloader.ui.theme.SaveItText
 import java.io.File
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun VideoPlayerScreen(
     filePath: String,
